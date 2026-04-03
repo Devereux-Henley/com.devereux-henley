7a19abf7639ad7f7902d275e3a499fc692d7a862
Devereux Henley <devereux.henley@gmail.com>
2026-04-03 00:03:02 -0500

    Add damage and AP split for each unit.

--- development/src/workspace.clj
+++ development/src/workspace.clj
@@ -2,11 +2,20 @@
   (:require
    [com.devereux-henley.rts-api.system :as system]
    [com.devereux-henley.rts-data.contract :as rts-data]
-   [com.devereux-henley.rts-api.db :as rts-db]))
+   [com.devereux-henley.rts-api.db :as rts-db]
+   [migratus.core :as migratus]))
+
+(def migratus-config
+  {:store         :database
+   :migration-dir rts-data/migration-dir
+   :db            rts-db/db-spec})
 
 (comment
   (system/go!)
   (system/halt!)
   (system/reload-views!)
   (system/restart!)
+  (migratus/migrate migratus-config)
+  (migratus/rollback migratus-config)
+  (migratus/reset migratus-config)
   (rts-data/seed-db rts-db/db-spec))
